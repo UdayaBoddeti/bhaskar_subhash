9ec12144379dba7ff949e60b2f259928db11d0fb
UdayaBoddeti <158701248+UdayaBoddeti@users.noreply.github.com>
2024-02-19 13:31:33 +0530

    Update test_prime_or_not.c

--- test_prime_or_not.c
+++ test_prime_or_not.c
@@ -6,23 +6,14 @@
 void main()
 {
 	char test_values[8][8] = {"0", "123", "17", "190", "145", "10Ab", "ab10", "ab"};
+	int result[8] = {-1, 0, 1, 0, 0, 0, -1, -1};
 	int count = 8, counter;
 
 	for (counter = 0;counter < count;counter++)
 	{
-		int prime_or_not = is_prime(test_values[counter]);
-
-		if (prime_or_not == 1)
+		if (is_prime(test__values[counter] != result[counter])
 		{
-			printf("%d is a prime number.\n", atoi(test_values[counter]));
-		}
-		else if (prime_or_not == -1)
-		{
-			printf("%s is invalid input.\n", test_values[counter]);
-		}
-		else
-		{
-			printf("%d is not a prime number.\n", atoi(test_values[counter]));
+			printf("%s: Failed.\n", test_values[counter]);
 		}
 	}
 }
